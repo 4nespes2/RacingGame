2d45d7e10d538b227d3984cfa13708b517bc15ff
4nespes2 <katena-polozkova@mail.ru>
2024-06-03 03:48:10 +0300

    add files

---
+++ RacingGame/RacingGame/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RacingGame
+{
+    public partial class Menu : Form
+    {
+        Thread th;
+        public Menu()
+        {
+            InitializeComponent();
+            form1 = new Form1(); 
+        }
+        Form1 form1;    
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //form1.Show();
+            //this.Hide();
+            th = new Thread(open);
+            th.SetApartmentState(ApartmentState.STA);   
+            th.Start();
+        }
+
+        public void open(object obj)
+        {
+            Application.Run(new Form1());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            th = new Thread(openr);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+        public void openr(object obj)
+        {
+            Application.Run(new Rools());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ RacingGame/RacingGame/Rools.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RacingGame
+{
+    public partial class Rools : Form
+    {
+        Thread th;
+        public Rools()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            th = new Thread(opengame);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+        public void opengame(object obj)
+        {
+            Application.Run(new Form1());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            th = new Thread(openmenu);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+        public void openmenu(object obj)
+        {
+            Application.Run(new Menu());
+        }
+    }
+}
